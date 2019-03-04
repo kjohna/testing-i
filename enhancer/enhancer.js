e4a430731d362ccaf7ba751457b4a254207c6b0c
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-04 16:17:19 -0700

    refactor test items, basic success method tests

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -1,10 +1,22 @@
 module.exports = {
   repair,
+  success,
 }
 
 function repair(item) {
   return {
     ...item,
     durability: 100
+  }
+}
+
+function success(item) {
+  let enhancementFinal = item.enhancement;
+  if (enhancementFinal < 20) {
+    enhancementFinal ++;
+  }
+  return {
+    ...item,
+    enhancement: enhancementFinal
   }
 }
--- enhancer/enhancer.spec.js
+++ enhancer/enhancer.spec.js
@@ -1,24 +1,66 @@
-const { repair } = require('./enhancer.js');
+const { repair, success } = require('./enhancer.js');
 
-// some items to test
-const brokenWeapon = {
-  name: "brokenWeapon",
-  type: "weapon",   // weapon or armor
-  durability: 90,   // 0 - 100, start = 100
+// *** some items to test
+// boilerplate items
+const weapon = {
+  name: "weapon",
+  type: "weapon",
+  durability: 100,  // 0 - 100, start = 100
   enhancement: 0,   // 0 - 20, start = 0
 }
-const brokenArmor = {
-  name: "brokenArmor",
-  type: "weapon",   // weapon or armor
-  durability: 80,   // 0 - 100, start = 100
-  enhancement: 20,   // 0 - 20, start = 0
+const armor = {
+  name: "armor",
+  type: "armor",
+  durability: 100,  // 0 - 100, start = 100
+  enhancement: 0,   // 0 - 20, start = 0
 }
+
+// for repair method
+const repairWeapon = {
+  ...weapon,
+  durability: 90,
+}
+const repairArmor = {
+  ...armor,
+  durability: 80,
+}
+
+// for success method
+const successWeaponBelow20 = {
+  ...weapon,
+  enhancement: 0,
+}
+const successWeaponAt20 = {
+  ...weapon,
+  enhancement: 20,
+}
+const successArmorBelow20 = {
+  ...armor,
+  enhancement: 0,
+}
+const successArmorAt20 = {
+  ...armor,
+  enhancement: 20,
+}
+
+// *** end test items
 
 describe('enhancer.js', () => {
   describe('repair()', () => {
     it('should restore durability to 100 for any item', () => {
-      expect(repair(brokenWeapon).durability).toBe(100);
-      expect(repair(brokenArmor).durability).toBe(100);
-    })
+      expect(repair(repairWeapon).durability).toBe(100);
+      expect(repair(repairArmor).durability).toBe(100);
+    });
+  });
+
+  describe('success()', () => {
+    it('should increase enhancement by 1', () => {
+      expect(success(successWeaponBelow20).enhancement).toBe(1);
+      expect(success(successArmorBelow20).enhancement).toBe(1);
+    });
+    it('should not increase enhancement beyond 20', () => {
+      expect(success(successWeaponAt20).enhancement).toBe(20);
+      expect(success(successArmorAt20).enhancement).toBe(20);
+    });
   });
 });
