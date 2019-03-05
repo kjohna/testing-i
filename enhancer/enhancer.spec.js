f23d5168a963fc675a97f4b920e38c50048a3887
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-04 17:21:52 -0700

    refactor: move test items close to test, better item names, add comments

--- enhancer/enhancer.spec.js
+++ enhancer/enhancer.spec.js
@@ -1,6 +1,5 @@
 const { repair, success, fail } = require('./enhancer.js');
 
-// *** some items to test
 // boilerplate items
 const weapon = {
   name: "weapon",
@@ -15,93 +14,45 @@ const armor = {
   enhancement: 0,   // 0 - 20, start = 0
 }
 
-// for repair method
-const repairWeapon = {
-  ...weapon,
-  durability: 90,
-}
-const repairArmor = {
-  ...armor,
-  durability: 80,
-}
-
-// for success method
-const successWeaponBelow20 = {
-  ...weapon,
-  enhancement: 0,
-}
-const successWeaponAt20 = {
-  ...weapon,
-  enhancement: 20,
-}
-const successArmorBelow20 = {
-  ...armor,
-  enhancement: 0,
-}
-const successArmorAt20 = {
-  ...armor,
-  enhancement: 20,
-}
-
-// for fail method
-const failWeaponE0D10 = {
-  ...weapon,
-  enhancement: 0,
-  durability: 10,
-}
-const failWeaponE0D5 = {
-  ...weapon,
-  enhancement: 0,
-  durability: 5,
-}
-const failWeaponE0D3 = {
-  ...weapon,
-  enhancement: 0,
-  durability: 3,
-}
-const failWeaponE15D20 = {
-  ...weapon,
-  enhancement: 15,
-  durability: 20,
-}
-const failWeaponE15D10 = {
-  ...weapon,
-  enhancement: 15,
-  durability: 10,
-}
-const failWeaponE15D3 = {
-  ...weapon,
-  enhancement: 15,
-  durability: 3,
-}
-const failWeaponE17D20 = {
-  ...weapon,
-  enhancement: 17,
-  durability: 20,
-}
-const failWeaponE16D20 = {
-  ...weapon,
-  enhancement: 16,
-  durability: 20,
-}
-
-// *** end test items
-
 describe('enhancer.js', () => {
   describe('repair()', () => {
     it('should restore durability to 100 for any item', () => {
+      const repairWeapon = {
+        ...weapon,
+        durability: 90,
+      }
       expect(repair(repairWeapon).durability).toBe(100);
+      const repairArmor = {
+        ...armor,
+        durability: 80,
+      }
       expect(repair(repairArmor).durability).toBe(100);
     });
   });
 
   describe('success()', () => {
-    it('should increase enhancement by 1', () => {
-      expect(success(successWeaponBelow20).enhancement).toBe(1);
-      expect(success(successArmorBelow20).enhancement).toBe(1);
+    it('should increase enhancement by 1, not beyond 20', () => {
+      const successWeapon0 = {
+        ...weapon,
+        enhancement: 0,
+      }
+      expect(success(successWeapon0).enhancement).toBe(1);
+      const successArmor0 = {
+        ...armor,
+        enhancement: 20,
+      }
+      expect(success(successArmor0).enhancement).toBe(20);
     });
     it('should not increase enhancement beyond 20', () => {
+      const successWeaponAt20 = {
+        ...weapon,
+        enhancement: 20,
+      }
       expect(success(successWeaponAt20).enhancement).toBe(20);
+      const successArmorAt20 = {
+        ...armor,
+        enhancement: 20,
+      }
       expect(success(successArmorAt20).enhancement).toBe(20);
     });
     // todo test updated name
@@ -109,20 +60,73 @@ describe('enhancer.js', () => {
 
   describe('fail()', () => {
     it('should reduce durability by 5 if enhancement is between 0 and 14', () => {
-        expect(fail(failWeaponE0D10).durability).toBe(5);
-        expect(fail(failWeaponE0D5).durability).toBe(0);
-        // make sure it doesn't go negative
-        expect(fail(failWeaponE0D3).durability).toBe(0);
+      // durability high
+      const failWeapon0 = {
+        ...weapon,
+        enhancement: 0,
+        durability: 10,
+      }
+      expect(fail(failWeapon0).durability).toBe(5);
+      // durability at 5
+      const failWeapon1 = {
+        ...weapon,
+        enhancement: 0,
+        durability: 5,
+      }
+      // durability low
+      expect(fail(failWeapon1).durability).toBe(0);
+      const failWeapon2 = {
+        ...weapon,
+        enhancement: 0,
+        durability: 3,
+      }
+      expect(fail(failWeapon2).durability).toBe(0);
     });
     it('should reduce durability by 10 if enhancement is greater than 14', () => {
-      expect(fail(failWeaponE15D20).durability).toBe(10);
-      expect(fail(failWeaponE15D10).durability).toBe(0);
-      // make sure it doesn't go negative
-      expect(fail(failWeaponE15D3).durability).toBe(0);
+      // durability high
+      const failWeapon0 = {
+        ...weapon,
+        enhancement: 15,
+        durability: 20,
+      }
+      expect(fail(failWeapon0).durability).toBe(10);
+      // durability at 10
+      const failWeapon1 = {
+        ...weapon,
+        enhancement: 15,
+        durability: 10,
+      }
+      expect(fail(failWeapon1).durability).toBe(0);
+      // durability low
+      const failWeapon2 = {
+        ...weapon,
+        enhancement: 15,
+        durability: 3,
+      }
+      expect(fail(failWeapon2).durability).toBe(0);
     });
     it('should reduce enhancement by 1 if enhancement is greater than 16', () => {
-      expect(fail(failWeaponE17D20).enhancement).toBe(16);
-      expect(fail(failWeaponE16D20).enhancement).toBe(16);
+      // enhancement greater than 16
+      const failWeapon0 = {
+        ...weapon,
+        enhancement: 17,
+        durability: 20,
+      }
+      expect(fail(failWeapon0).enhancement).toBe(16);
+      // enhancement at 16
+      const failWeapon1 = {
+        ...weapon,
+        enhancement: 16,
+        durability: 20,
+      }
+      expect(fail(failWeapon1).enhancement).toBe(16);
+      // enhancement below 16
+      const failWeapon2 = {
+        ...weapon,
+        enhancement: 15,
+        durability: 20,
+      }
+      expect(fail(failWeapon2).enhancement).toBe(15);
     });
     // todo test updated name
   })
